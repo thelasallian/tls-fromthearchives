80ea823ca3f22e4abb0c26c6592bdef992d928a8
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2022-06-02 05:43:49 +0800

    Added Footer Styles

--- index.php
+++ index.php
@@ -13,30 +13,35 @@
     <title>The LaSallian: From the Archives</title>
   </head>
   <body>
-    <h1>Hello, world!</h1> <!-- Temporary -->
+    <h1>🦕🦕🦕</h1> <!-- Temporary -->
 
 
     <!-- Footer -->
     <footer class="py-5">
       <div class="container">
         <div class="row">
-          <div class="col-lg-6 col-sm-12 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
-            <!-- Logo -->
-            <img src="images/old-tls-wordmark.png" alt="TLS Name Logo" class="footer-logo mb-4">
-            <!-- Logo Heading -->
-            <p class="logo-heading"><strong>Archives Website</strong></p>
-            <!-- Footer Text -->
+          <div class="left col-xs-12 col-lg-5 col-sm-5 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
+            <figure>
+              <!-- Logo -->
+              <img src="images/old-tls-wordmark.png" alt="TLS Name Logo" class="footer-logo mb-4">
+              <!-- Logo Heading -->
+              <figcaption class="footer-heading"><strong>Archives Website</strong></figcaption>
+            </figure>
+          </div>
+          <div class="col-xs-12 col-lg-6 col-sm-6 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
+            <!-- Description -->
             <p class="footer-description"><strong>The LaSallian</strong> is the official student publication of De La Salle University. It is of the
                                                                          students, by the students, and for the students. Our student writers, photographers,
                                                                          videographers, and web managers are committed to the 61-year tradition of
                                                                          journalistic excellence and issue-oriented critical thinking.</p>
             <!-- Link to Main Website -->
-            <p class="footer-link-main-site"> href="https://thelasallian.com" <strong>Go To Main Website ></strong></p>
+            <p class="footer-link-main-site"><a href="https://thelasallian.com"><strong>Go to Main Website ></strong></a></p>
             <!-- Website Developers -->
-            <p class="footer-developers">Website by Angelo Guerra & Ronn Parcia</p>
+            <p class="footer-developers"><strong>Website by Angelo Guerra & Ronn Parcia</strong></p>
           </div>
         </div>
       </div>
+    </footer>
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
