1ca6a6afb1b9c33b2a3ce3dfb458eae3d99b1d71
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-06-07 21:33:31 +0800

    Fixed indentation

--- index.php
+++ index.php
@@ -11,12 +11,12 @@
 
     <!-- Title -->
     <title>The LaSallian: From the Archives</title>
-  </head>
-  <body>
+</head>
+<body>
     <h1>Hello, world!</h1> <!-- Temporary -->
 
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
