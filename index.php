1ab8d4c45bdd3fcd016fc04b3bcd9d7e9c989807
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-07-19 20:26:35 +0800

    Added favicons

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     <link rel="stylesheet" rel="stylesheet" href="styles/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <!-- Favicons -->
+    <link rel="icon" href="https://thelasallian.com/wp-content/uploads/2020/06/LOGO.png" sizes="32x32" />
+    <link rel="icon" href="https://thelasallian.com/wp-content/uploads/2020/06/LOGO.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://thelasallian.com/wp-content/uploads/2020/06/LOGO.png" />
+
     <!-- Title -->
     <title>The LaSallian: From the Archives</title>
   </head>
