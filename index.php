0acacbdf02f8e4a686c2dc4085839f4507555e1c
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2022-07-21 02:51:18 +0800

    Edited header title

--- index.php
+++ index.php
@@ -16,80 +16,24 @@
 
     <!-- Header -->
     <header>
-      <div class="container d-flex flex-column align-items-center">
-        <div class="row">
-          <div class="header-title col-xs-12 col-lg-4 col-sm-4 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
-            <!-- Logo -->
-            <img src="images/old-tls-star.png" alt="The LaSallian FTA Star" class="header-logo mb-4">
-            <!-- Header Title -->
-            <h3 class="header-title-top">From the</h3>
-            <h1 class="header-title-bottom"><strong>Archives</strong></h1>
-            <!-- Header Description -->
-            <p class="header-description">This is a description of the FTA special. Plura
-                                          mihi bona sunt, inclinet, amari petere vellent. Ab
-                                          illo tempore, ab est sed immemorabili. Ullamco
-                                          laboris nisi ut aliquid ex ea commodi consequat. </p>
-          </div>
-          
-          <div class="header-article col-md-4 col-xs-7 col-lg-7 col-sm-6 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
-            <div class="card">
-              <div class="row">
-
-                <div class="col-md-6 col-xs-12 col-lg-6 col-sm-6 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
-                  <div class="card-body">
-                    <h5 class="article-category"><strong>UNIVERSITY</strong></h5>
-                    <h4 class="article-title"><strong>From the Archives: Quito awarded professional lecture</strong></h4>
-                  
-                    <div class="d-flex flex-row">
-                      <p class="article-author"><strong>Mica Buencamino</strong></p>
-                      <p class="article-date">2 years ago</p>
-                    </div>
-                    
-                    <p class="article-intro">The ceremonies began at 4:30 in the afternoon with
-                                             opening remarks given by the Dean of Arts and Sciences, 
-                                             Exaltacion C. Ramos, Bro. Andrew Gonzalez, FSC, DLSU...</p>
-                    <a href="https://thelasallian.com/2020/12/26/from-the-archives-quito-awarded-professorial-lecture/" class="stretched-link"></a>
-                  </div>
-                </div>
-
-                <div class="col-md-6 col-xs-12 col-lg-6 col-sm-6 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
-                  <img class="card-img" src="images/article-visual.jpg" alt="Article Visual">
-                </div>
-              </div>
-            </div>
-          </div>
+      <div class="row">
+        <!-- Header Title -->
+        <div class="header-title col-sm-12 col-lg-6 d-flex flex-column justify-content-center align-items-center align-items-lg-start text-lg-start text-center">
+          <!-- Logo -->
+          <img src="images/old-tls-star.png" alt="The LaSallian FTA Star" class="header-logo mb-4">
+          <!-- Header Title -->
+          <h3 class="header-title-top">From the</h3>
+          <h1 class="header-title-bottom"><strong>Archives</strong></h1>
+          <!-- Header Description -->
+          <p class="header-description">Since its founding in 1960, The LaSallian has been committed to chronicling
+                                        chronicling the history of De La Salle University and Filipino society.
+                                        Explore the publication's articles, photographs, artworks, and videos to
+                                        truly unlock the essence of the bastion of issue-oriented critical thinking. </p>
         </div>
       </div>
     </header>
 
     <h1 class="text-center">🦕🦕🦕</h1> <!-- Temporary -->
-
-    <!-- Footer -->
-    <footer class="py-5">
-      <div class="container">
-        <div class="row">
-          <div class="left col-xs-12 col-lg-5 col-sm-5 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
-            <figure>
-              <!-- Logo -->
-              <img src="images/old-tls-wordmark.png" alt="TLS Name Logo" class="footer-logo mb-4">
-              <!-- Logo Heading -->
-              <figcaption class="footer-heading"><strong>Archives Website</strong></figcaption>
-            </figure>
-          </div>
-          <div class="col-xs-12 col-lg-6 col-sm-6 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
-            <!-- Description -->
-            <p class="footer-description"><strong>The LaSallian</strong> is the official student publication of De La Salle University. It is of the
-                                                                         students, by the students, and for the students. Our student writers, photographers,
-                                                                         videographers, and web managers are committed to the 61-year tradition of
-                                                                         journalistic excellence and issue-oriented critical thinking.</p>
-            <!-- Link to Main Website -->
-            <p class="footer-link-main-site"><a href="https://thelasallian.com"><strong>Go to Main Website ></strong></a></p>
-            <!-- Website Developers -->
-            <p class="footer-developers"><strong>Website by Angelo Guerra & Ronn Parcia</strong></p>
-          </div>
-        </div>
-      </div>
-    </footer>
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
