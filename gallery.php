463f04b242045cc925c5a763e2ed3a994ab10358
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-07-24 00:50:10 +0800

    Removed extra code and implemented foreach loop template for photo cards

--- gallery.php
+++ gallery.php
@@ -24,21 +24,17 @@
 
 <body>
     
-    <ol>
-        <?php
-        $allPhotos = json_decode(file_get_contents("https://github.com/ronnparcia/tls-fta-scans/blob/main/all-photos.json?raw=true"), true);
-        foreach ($allPhotos as $photo):
-            $date = $photo["date"];
-            $caption = $photo["caption"];
-            $imageURL = $photo["image-url"];
-        ?>
-            <li>
-                <?php echo $date; ?><br/>
-                <?php echo $caption; ?><br/>
-                <?php echo $imageURL; ?>
-            </li>
-        <?php endforeach; ?>
-    </ol>
+    <?php
+    $allPhotos = json_decode(file_get_contents("https://github.com/ronnparcia/tls-fta-scans/blob/main/all-photos.json?raw=true"), true);
+    foreach ($allPhotos as $photo):
+        $date = $photo["date"];
+        $caption = $photo["caption"];
+        $imageURL = $photo["image-url"];
+    ?>
+        
+        <!-- Insert Code for Photo Cards Here -->
+
+    <?php endforeach; ?>
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
