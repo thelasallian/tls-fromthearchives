49ad3f7c838163914a06b228149e6c9f22c7ef11
Ronn Parcia <ronn_parcia@dlsu.edu.ph>
2022-07-24 14:44:21 +0800

    Added 'rawr' to check if pushes are... pushed.

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
         </div>
     </header>
 
-    <h1 class="text-center">🦕🦕🦕</h1> <!-- Temporary -->
+    <h1 class="text-center">rawr🦕🦕🦕</h1> <!-- Temporary -->
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
